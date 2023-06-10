96904c24e3ab60e841178c6a784d970ed2f1409e
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2023-06-10 12:25:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,9 @@ resource "aws_instance" "devops_01_docker-nginx" {
     sudo yum install -y yum-utils
     sudo yum install git -y
     sudo yum install docker -y
-    sudo service docker start
+    
   EOF
+#   sudo service docker start
 #     sudo usermod -a -G docker ec2-user
 #     sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 #     sudo chmod +x /usr/local/bin/docker-compose
